1a77d41898c3e7ba0b5a57ad6ce7a6f6fb9170a6
Trần Minh Thiện <nhoktmt2002@gmail.com>
2024-10-31 21:21:35 +0700

    [Change] Data for FinalQuoteTable

--- src/pages/Quote/DetailedQuotation/Table/FinalQuotationTable.tsx
+++ src/pages/Quote/DetailedQuotation/Table/FinalQuotationTable.tsx
@@ -11,8 +11,11 @@ const FinalQuotationTable: React.FC<FinalQuotationTableProps> = ({ items }) => {
       <table className="min-w-full bg-white border border-gray-200">
         <thead>
           <tr>
-            <th className="px-4 py-2 border text-center">Tên hạng mục</th>
+            <th className="px-4 py-2 border text-center">Tên công trình</th>
             <th className="px-4 py-2 border text-center">Loại</th>
+            <th className="px-4 py-2 border text-center">Hệ số</th>
+            <th className="px-4 py-2 border text-center">Ngày tạo</th>
+            <th className="px-4 py-2 border text-center">Tên hạng mục</th>
             <th className="px-4 py-2 border text-center">Đơn vị</th>
             <th className="px-4 py-2 border text-center">Khối lượng</th>
             <th className="px-4 py-2 border text-center">Đơn giá nhân công</th>
@@ -25,24 +28,63 @@ const FinalQuotationTable: React.FC<FinalQuotationTableProps> = ({ items }) => {
         </thead>
         <tbody>
           {items.map((item) => (
-            <tr key={item.Id}>
-              <td className="px-4 py-2 border text-center">{item.Name}</td>
-              <td className="px-4 py-2 border text-center">{item.Type}</td>
-              <td className="px-4 py-2 border text-center">{item.Unit}</td>
-              <td className="px-4 py-2 border text-center">{item.Weight}</td>
-              <td className="px-4 py-2 border text-center">
-                {item.UnitPriceLabor?.toLocaleString() || 'N/A'} VNĐ
-              </td>
-              <td className="px-4 py-2 border text-center">
-                {item.UnitPriceRough?.toLocaleString() || 'N/A'} VNĐ
-              </td>
-              <td className="px-4 py-2 border text-center">
-                {item.TotalPriceLabor?.toLocaleString() || 'N/A'} VNĐ
-              </td>
-              <td className="px-4 py-2 border text-center">
-                {item.TotalPriceRough?.toLocaleString() || 'N/A'} VNĐ
-              </td>
-            </tr>
+            <React.Fragment key={item.Id}>
+              <tr>
+                <td
+                  className="px-4 py-2 border text-center"
+                  rowSpan={item.QuotationItems.length + 1}
+                >
+                  {item.ContructionName}
+                </td>
+                <td
+                  className="px-4 py-2 border text-center"
+                  rowSpan={item.QuotationItems.length + 1}
+                >
+                  {item.Type}
+                </td>
+                <td
+                  className="px-4 py-2 border text-center"
+                  rowSpan={item.QuotationItems.length + 1}
+                >
+                  {item.Coefficient}
+                </td>
+                <td
+                  className="px-4 py-2 border text-center"
+                  rowSpan={item.QuotationItems.length + 1}
+                >
+                  {item.InsDate || 'null'}
+                </td>
+              </tr>
+              {item.QuotationItems.map((quotationItem) => (
+                <tr key={quotationItem.Id}>
+                  <td className="px-4 py-2 border text-center">
+                    {quotationItem.Name}
+                  </td>
+                  <td className="px-4 py-2 border text-center">
+                    {quotationItem.Unit}
+                  </td>
+                  <td className="px-4 py-2 border text-center">
+                    {quotationItem.Weight}
+                  </td>
+                  <td className="px-4 py-2 border text-center">
+                    {quotationItem.UnitPriceLabor?.toLocaleString() || 'null'}{' '}
+                    VNĐ
+                  </td>
+                  <td className="px-4 py-2 border text-center">
+                    {quotationItem.UnitPriceRough?.toLocaleString() || 'null'}{' '}
+                    VNĐ
+                  </td>
+                  <td className="px-4 py-2 border text-center">
+                    {quotationItem.TotalPriceLabor?.toLocaleString() || 'null'}{' '}
+                    VNĐ
+                  </td>
+                  <td className="px-4 py-2 border text-center">
+                    {quotationItem.TotalPriceRough?.toLocaleString() || 'null'}{' '}
+                    VNĐ
+                  </td>
+                </tr>
+              ))}
+            </React.Fragment>
           ))}
         </tbody>
       </table>
