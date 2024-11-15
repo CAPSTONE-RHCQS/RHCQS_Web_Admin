597066a480ac038a03ccbef95fbea76e77bf47a2
Trần Minh Thiện <nhoktmt2002@gmail.com>
2024-11-16 02:34:57 +0700

    Update HouseDesignDetailManager.tsx

--- src/pages/Manager/HouseDesignDrawing/HouseDesignDetailManager.tsx
+++ src/pages/Manager/HouseDesignDrawing/HouseDesignDetailManager.tsx
@@ -109,44 +109,46 @@ const HouseDesignDetailManager: React.FC = () => {
 
   return (
     <>
-      <div className="p-4  mx-auto">
+      <div className="p-4 mx-auto">
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-3xl font-bold text-center">
             Chi tiết bản vẽ thiết kế
           </h2>
-          <div
-            onMouseEnter={showMenu}
-            onMouseLeave={hideMenu}
-            className="relative"
-          >
-            <FiMoreVertical className="text-xl text-black dark:text-white cursor-pointer" />
-            {menuVisible && (
-              <div
-                className="absolute right-2 top-2 mt-2 w-56 bg-white border border-gray-200 rounded-lg shadow-lg z-50 transition-opacity duration-300 ease-in-out"
-                style={{ opacity: menuVisible ? 1 : 0 }}
-              >
-                <div className="py-2">
-                  <a
-                    href="#"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      if (selectedVersionId) {
-                        setIsModalOpen(true);
-                      } else {
-                        toast.error(
-                          'Vui lòng chọn một phiên bản trước khi phê duyệt.',
-                        );
-                      }
-                    }}
-                    className="flex items-center px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
-                  >
-                    <FaCheck className="mr-2" />
-                    Phê duyệt bản vẽ
-                  </a>
+          {designDetail.Status === 'Reviewing' && (
+            <div
+              onMouseEnter={showMenu}
+              onMouseLeave={hideMenu}
+              className="relative"
+            >
+              <FiMoreVertical className="text-xl text-black dark:text-white cursor-pointer" />
+              {menuVisible && (
+                <div
+                  className="absolute right-2 top-2 mt-2 w-56 bg-white border border-gray-200 rounded-lg shadow-lg z-50 transition-opacity duration-300 ease-in-out"
+                  style={{ opacity: menuVisible ? 1 : 0 }}
+                >
+                  <div className="py-2">
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        if (selectedVersionId) {
+                          setIsModalOpen(true);
+                        } else {
+                          toast.error(
+                            'Vui lòng chọn một phiên bản trước khi phê duyệt.',
+                          );
+                        }
+                      }}
+                      className="flex items-center px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-green-600 transition-colors duration-200"
+                    >
+                      <FaCheck className="mr-2" />
+                      Phê duyệt bản vẽ
+                    </a>
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
+              )}
+            </div>
+          )}
         </div>
         <WorkDetailStatusTracker currentStatus={designDetail.Status} />
         <div className="flex items-start">
