d37ba09a0a78d2c66ca9b656cc18e36e2018c28b
Trần Minh Thiện <nhoktmt2002@gmail.com>
2024-11-18 03:22:57 +0700

    [Update] Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -380,52 +380,6 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 </NavLink>
               </li> */}
 
-                  {/* <!-- Menu Item detailed-quotation --> */}
-                  <li>
-                    <NavLink
-                      to="/detailed-quotation"
-                      className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-teal-300 dark:hover:bg-meta-4 ${
-                        pathname.includes('detailed-quotation') &&
-                        'bg-teal-300 dark:bg-meta-4'
-                      }`}
-                    >
-                      <svg
-                        className="fill-current"
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="20px"
-                        viewBox="0 -960 960 960"
-                        width="20px"
-                        fill="#5f6368"
-                      >
-                        <path d="M444-192h72v-48h12q20.4 0 34.2-13.8Q576-267.6 576-288v-72q0-20.4-13.8-34.2Q548.4-408 528-408h-96v-72h144v-48h-60v-48h-72v48h-12q-20.4 0-34.2 13.8Q384-500.4 384-480v72q0 20.4 13.8 34.2Q411.6-360 432-360h96v72H384v48h60v48ZM263.72-96Q234-96 213-117.15T192-168v-624q0-29.7 21.15-50.85Q234.3-864 264-864h312l192 192v504q0 29.7-21.16 50.85Q725.68-96 695.96-96H263.72ZM528-624v-168H264v624h432v-456H528ZM264-792v189-189 624-624Z" />
-                      </svg>
-                      Tạo báo giá chi tiết
-                    </NavLink>
-                  </li>
-
-                  {/* <!-- Menu Item CreateQuote --> */}
-                  <li>
-                    <NavLink
-                      to="/create-Contract-Design"
-                      className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-teal-300 dark:hover:bg-meta-4 ${
-                        pathname.includes('createquote') &&
-                        'bg-teal-300 dark:bg-meta-4'
-                      }`}
-                    >
-                      <svg
-                        className="fill-current"
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="20px"
-                        viewBox="0 -960 960 960"
-                        width="20px"
-                        fill="#5f6368"
-                      >
-                        <path d="M240-96q-40 0-68-28t-28-68v-144h96v-528h576v660q0 45-31.5 76.5T708-96H240Zm467.79-72q15.21 0 25.71-10.35T744-204v-588H312v456h360v132q0 15.3 10.29 25.65Q692.58-168 707.79-168ZM360-600v-72h336v72H360Zm0 120v-72h336v72H360ZM240-168h360v-96H216v72q0 10.2 6.9 17.1 6.9 6.9 17.1 6.9Zm0 0h-24 384-360Z" />
-                      </svg>
-                      Tạo hợp đồng thiết kế
-                    </NavLink>
-                  </li>
-
                   {/* <!-- Menu Item BlogList --> */}
                   <li>
                     <NavLink
