7eb3974b2c12e3bc5d128774c77bece73e176967
Trần Minh Thiện <nhoktmt2002@gmail.com>
2024-11-19 05:09:12 +0700

    [Update] UI House Template

--- src/pages/Manager/HouseTemplate/components/PackageHouseList.tsx
+++ src/pages/Manager/HouseTemplate/components/PackageHouseList.tsx
@@ -12,21 +12,21 @@ const PackageHouseList: React.FC<PackageHouseListProps> = ({
 }) => {
   return (
     <div className="mt-6">
-      <Typography variant="h5" className="font-bold text-gray-800">
+      <Typography variant="h5" className="font-bold text-primary">
         Danh sách gói xây dựng nhà:
       </Typography>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {packageHouses.map((packageHouse) => (
           <div
             key={packageHouse.Id}
-            className="border p-4 rounded-lg shadow-md"
+            className="border p-4 rounded-lg shadow-md transition-transform transform hover:scale-105"
           >
             <img
               src={packageHouse.ImgUrl || defaultImageHouseTemplateUrl}
               alt={packageHouse.PackageName}
               className="w-full h-32 object-cover rounded-md mb-2"
             />
-            <Typography variant="h6" className="font-bold text-gray-800">
+            <Typography variant="h6" className="font-bold text-primary">
               {packageHouse.PackageName}
             </Typography>
             <Typography className="text-gray-600">
