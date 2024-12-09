4db1baa255d9ccbb4e0259706b6b7cf08c8a35de
Trần Minh Thiện <nhoktmt2002@gmail.com>
2024-12-09 13:06:41 +0700

    [Fix] Price Contract

--- src/pages/SalesStaff/Contract/ContractDetailStaff.tsx
+++ src/pages/SalesStaff/Contract/ContractDetailStaff.tsx
@@ -232,7 +232,7 @@ const ContractDetailStaff = () => {
       })),
       batchPaymentRequests: batchPaymentRequests.map((request) => ({
         numberOfBatches: request.NumberOfBatches,
-        price: request.Price,
+        price: request.Price * request.Percents,
         paymentDate: request.PaymentDate,
         paymentPhase: request.PaymentPhase,
         percents: request.Percents,
